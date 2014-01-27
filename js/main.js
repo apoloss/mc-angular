6138bf079afa8df144aacde54e0ecb98d82ffaaa
apoloss <politoagustin@wedidt.com>
2014-01-27 16:41:32 -0300

    Instant search

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 var app = angular.module('ttsApp',['ngResource']);
 
 app.controller("AppCtrl", ['$http','$scope','$resource', function($http, $scope, $resource) {
-  $scope.makeCall = function(resc) {
-	$http.defaults.useXDomain = true;
-  	Something = $resource("https://api.mercadolibre.com/sites/MLA/search/:qs", {qs: "@qs"});
-  	$scope.api = Something.get({q:resc});
+  $scope.changed = function(e) {
+    console.log(e);
   };
+  $scope.$watch('search', function(querystr) {
+    Something = $resource("https://api.mercadolibre.com/sites/MLA/search/:qs", {q: "@q"});
+    $scope.api = Something.get({q:querystr});
+  });
 
 }]);
